2e8fb348c342e739a2a227e2460402480873bc95
Ivan Urquijo <urquijo123@hotmail.com>
2022-03-04 12:21:34 -0500

    sds

--- Test1lambdas/Function.cs
+++ Test1lambdas/Function.cs
@@ -16,6 +16,6 @@ public class Function
     /// <returns></returns>
     public string FunctionHandler(string input, ILambdaContext context)
     {
-        return input.ToUpper() + "Nadnjs dfad";
+        return input.ToUpper() + "Nadnjs dfad qs";
     }
 }
